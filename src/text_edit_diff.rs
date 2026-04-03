c704d21b7fe4f7d544f211ae24fd192ec66d8f1b
zwzollers <zwzollers@gmail.com>
2026-04-03 18:07:45 -0500

    text edit diff

---
+++ src/text_edit_diff.rs
@@ -0,0 +1,18 @@
+use egui::{TextBuffer, Ui};
+
+/// This file is a recreation of egui::TextEdit that includes support for text diffs
+/// 
+
+struct TextEditDiffResult {
+    
+}
+
+struct TextEditDiff {
+    
+}
+
+impl TextEditDiff {
+    pub fn show(self, ui: &mut Ui, text: &mut dyn TextBuffer ) -> TextEditDiffResult {
+        
+    }
+}
