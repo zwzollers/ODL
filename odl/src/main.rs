359d7833051a72177ef059d1713e2acf0f8c0526
zwzollers <zwzollers@gmail.com>
2026-07-16 21:04:11 -0500

    working runtime

--- odl/src/main.rs
+++ odl/src/main.rs
@@ -1,31 +1,134 @@
-use parser::{syntax, runtime::Runtime};
-use petgraph::graph::DiGraph;
+use parser::{syntax, runtime::{Runtime, Instr, HeapKind}};
+use petgraph::csr::Csr;
+use std::time::Instant;
 
 fn main() {
     let mut errors = Vec::new();
 
+    let now = Instant::now();
+
     let expr = syntax::ASTParser::new()
         .parse( &mut errors,
             r#"
-            //struct point {
-            //    x: u32,
-            //    y: u32,
-            //}
+            mod top (
+                a: in bool,
+                x: out num,
+                y: out num,
+            ) {
+                let i = 6;
+                if a {
+                    foo(a:6, x:k);
+                    let x = (j * i) + j * k;
+                    let j = 10;
+                } -> (x:x)
+                else {
+                    let x = 10;
+                } -> (x:x)
 
-            enum dir {
-                North,
-                East,
-                South,
-                West,
+                y = i;
             }
 
-            mod test (
-
+            mod foo (
+                a: in num,
+                x: out num,
             ) {
-                //let x = [hi, ho, 5.5*5, "4"];
-            }     
+                x = a + 4;
+            }
             "#);
 
-    println!("{expr:#?}, {errors:#?}");
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
+
+    //println!("{expr:#?}, {errors:#?}");
+
+
+    let mut g = Csr::<Instr, ()>::new();
+
+    // FOO
+    let foo_x = g.add_node(Instr::LOA { addr: 1 });
+    let add_0 = g.add_node(Instr::ADD);
+    g.add_edge(foo_x, add_0, ());
+    let foo_a = g.add_node(Instr::LOA { addr: 0 });
+    g.add_edge(add_0, foo_a, ());
+    let psh_4 = g.add_node(Instr::PSH { data: 4 });
+    g.add_edge(add_0, psh_4, ());
+
+    // TOP
+    let top_x = g.add_node(Instr::LOA { addr: 3 });
+    let if_c = g.add_node(Instr::SCP { addr: 1, alloc: 1 });
+    g.add_edge(top_x, if_c, ());
+    
+
+    let speek_0 = g.add_node(Instr::SPK { n: 1 });
+    let top_a = g.add_node(Instr::LOA { addr: 0 });
+    g.add_edge(speek_0, top_a, ());
+
+    let if_f = g.add_node(Instr::SCP { addr: 0, alloc: 1 });
+    let if_x = g.add_node(Instr::LOA { addr: 0 });
+    g.add_edge(if_f, if_x, ());
+    let psh_10 = g.add_node(Instr::PSH { data: 10 });
+    g.add_edge(if_x, psh_10, ());
+
+    let if_t = g.add_node(Instr::SCP { addr: 0, alloc: 4 });
+    let if_x = g.add_node(Instr::LOA { addr: 0 });
+    g.add_edge(if_t, if_x, ());
+    let add_0 = g.add_node(Instr::ADD);
+    g.add_edge(if_x, add_0, ());
+
+    let mul_0 = g.add_node(Instr::MUL);
+    g.add_edge(add_0, mul_0, ());
+
+    let speek = g.add_node(Instr::SPK { n: 2 });
+    g.add_edge(mul_0, speek, ());
+    let top_i = g.add_node(Instr::LOA { addr: 2 });
+    g.add_edge(speek, top_i, ());
+    let psh_6 = g.add_node(Instr::PSH { data: 6 });
+    g.add_edge(top_i, psh_6, ());
+
+    let if_j = g.add_node(Instr::LOA { addr: 2 });
+    g.add_edge(mul_0, if_j, ());
+    let psh_10 = g.add_node(Instr::PSH { data: 10 });
+    g.add_edge(if_j, psh_10, ());
+
+    let mul_1 = g.add_node(Instr::MUL);
+    g.add_edge(add_0, mul_1, ());
+    g.add_edge(mul_1, if_j, ());
+
+    let if_k = g.add_node(Instr::LOA { addr: 3 });
+    g.add_edge(mul_1, if_k, ());
+    let foo = g.add_node(Instr::SCP { addr: 1, alloc: 2 });
+    g.add_edge(if_k, foo, ());
+    let cal = g.add_node(Instr::CALL { node: foo_x });
+    g.add_edge(foo, cal, ());
+    let pop = g.add_node(Instr::POP);
+    g.add_edge(cal, pop, ());
+    let foo_a = g.add_node(Instr::LOA { addr: 0 });
+    g.add_edge(pop, foo_a, ());
+    let psh_6 = g.add_node(Instr::PSH { data: 6 });
+    g.add_edge(foo_a, psh_6, ());
+
+    let _if = g.add_node(Instr::IF { cond: speek_0, t: if_t, f: if_f });
+    g.add_edge(if_c, _if, ());
+    let top_y = g.add_node(Instr::LOA { addr: 4 });
+    g.add_edge(top_y, top_i, ());
+
+    let mut rt = Runtime::new(g);
+
+    rt.alloc(5);
+    rt.mem_wr(0, HeapKind::Num(1));
+    rt.entry_point = top_x;
+
+
+    let now = Instant::now();
+
+    rt.run();
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
+    
+
+    println!("{:?}", rt.mem_rd(3));
+
+    println!("{:?}", rt.heap);
 
 }
--- parser/src/runtime.rs
+++ parser/src/runtime.rs
@@ -1,34 +1,34 @@
-use std::{collections::HashMap, default};
-
-use petgraph::graph::NodeIndex;
-use petgraph::{data::DataMapMut, graph::DiGraph};
-use petgraph::visit::NodeIndexable;
-
-use crate::ast::{self, AST, Module, Statement, TopStructre};
+use petgraph::csr::Csr;
 
 
-type RTGraph<N, E> = DiGraph<N, E, u32>;
+type RTGraph<N, E> = Csr<N, E>;
 
 #[derive(Default)]
 pub struct Runtime {
     graph:     RTGraph<Instr, ()>,
-    heap:      Vec<HeapKind>,
+    pub heap:      Vec<HeapKind>,
     alloc_ptr: u32,
-    node_ptr:  NodeIndex,
+    node_ptr:  u32,
     scope_st:  Vec<u32>,
     scope_st_ptr: u32,
     instr_st:  Vec<Instr>,
     data_st:   Vec<u32>,  
-    entry_point: u32,
+    pub entry_point: u32,
     run_direction: RTDirection,
 }
 
 impl Runtime {
+    pub fn new(g: RTGraph<Instr, ()>) -> Self {
+        Runtime { graph: g, ..Default::default() }
+    }
     pub fn run(&mut self) {
-        let start_node = self.graph.from_index(self.entry_point as usize);
 
-        self.instr_st.push(self.graph[start_node].clone());
-        self.node_ptr = start_node;
+        self.node_ptr = self.entry_point;
+        self.scope_st.push(0);
+        self.scope_st_ptr = 0;
+
+        self.push_instr(self.graph[self.entry_point].clone());
+
 
         while self.instr_st.len() != 0 {
             match self.run_direction {
@@ -37,7 +37,7 @@ impl Runtime {
                     instr.exe(self);
                 }
                 RTDirection::Traverse => {
-                    let nbors: Vec<NodeIndex> = self.graph.neighbors_directed(self.node_ptr, petgraph::Direction::Outgoing).collect();
+                    let nbors = self.graph.neighbors_slice(self.node_ptr);
                     
                     if nbors.len() == 0 {
                         self.run_direction = RTDirection::Execute;
@@ -54,22 +54,39 @@ impl Runtime {
         instr.add(self);
 
     }
+    pub fn alloc(&mut self, num: u32) {
+
+        self.alloc_ptr += num;
+        for _ in 0..num {
+            self.heap.push(HeapKind::None);
+        }
+    }
+    pub fn mem_wr(&mut self, addr: u32, val: HeapKind) {
+        self.heap[addr as usize] = val;
+    }
+    pub fn mem_rd(&self, addr: u32) -> HeapKind {
+        self.heap[addr as usize].clone()
+    }
+    pub fn set_node_ptr(&mut self, node: &u32) {
+        self.node_ptr = *node;
+        self.push_instr(self.graph[self.node_ptr].clone());
+    }
 }
 
-#[derive(Clone)]
-enum Instr {
+#[derive(Clone, Debug)]
+pub enum Instr {
     ADD,
     SUB,
     MUL,
     DIV,
-    RET{node: NodeIndex, path: u32},
+    RET{node: u32, path: u32},
     SCP{addr: u32, alloc: u32},
     SPK{n: u32},
     POP,
     PSH{data: u32},
     LOA{addr: u32}, 
-    CALL{node: NodeIndex},
-    IF{cond: NodeIndex, t: NodeIndex, f: NodeIndex},
+    CALL{node: u32},
+    IF{cond: u32, t: u32, f: u32},
 }
 
 impl Instr {
@@ -82,17 +99,14 @@ impl Instr {
                 let heap_addr = (rt.scope_st.last().unwrap() + addr) as usize;
                 rt.heap[heap_addr] = HeapKind::Num(rt.alloc_ptr);
                 rt.scope_st.push(rt.alloc_ptr);
-                rt.alloc_ptr += alloc;
-
-                for _ in 0..*alloc {
-                    rt.heap.push(HeapKind::None);
-                }
+                
+                rt.alloc(*alloc);
             },
             Instr::SPK { n } => {
                 rt.scope_st_ptr += n;
             },
             Instr::LOA { addr } => {
-                let scope_addr = rt.scope_st[rt.scope_st.len() - rt.scope_st_ptr as usize];
+                let scope_addr = rt.scope_st[rt.scope_st.len() - 1 - rt.scope_st_ptr as usize];
                 let heap_data = &rt.heap[(scope_addr + addr) as usize];
 
                 if let HeapKind::Num(n) = heap_data {
@@ -102,39 +116,49 @@ impl Instr {
                 }
             },
             Instr::IF {cond, .. } => {
-                rt.node_ptr = *cond;
+                rt.set_node_ptr(cond);
+                rt.run_direction = RTDirection::Traverse;
             },
             _ => ()
         }
     }
     pub fn exe(&self, rt: &mut Runtime) {
-                match &self {
+        match &self {
             Instr::ADD => {
-                let a = rt.data_st.pop().unwrap();
-                let b = rt.data_st.pop().unwrap();
+                let a = unsafe { rt.data_st.pop().unwrap_unchecked() };
+                let b = unsafe { rt.data_st.pop().unwrap_unchecked() };
 
                 rt.data_st.push(a + b);
             },
             Instr::SUB => {
-                let a = rt.data_st.pop().unwrap();
-                let b = rt.data_st.pop().unwrap();
+                let a = unsafe { rt.data_st.pop().unwrap_unchecked() };
+                let b = unsafe { rt.data_st.pop().unwrap_unchecked() };
 
                 rt.data_st.push(a - b);
             },
             Instr::MUL => {
-                let a = rt.data_st.pop().unwrap();
-                let b = rt.data_st.pop().unwrap();
+                let a = unsafe { rt.data_st.pop().unwrap_unchecked() };
+                let b = unsafe { rt.data_st.pop().unwrap_unchecked() };
 
                 rt.data_st.push(a * b);
             },
             Instr::DIV => {
-                let a = rt.data_st.pop().unwrap();
-                let b = rt.data_st.pop().unwrap();
+                let a = unsafe { rt.data_st.pop().unwrap_unchecked() };
+                let b = unsafe { rt.data_st.pop().unwrap_unchecked() };
 
                 rt.data_st.push(a / b);
             },
             Instr::RET { node, path } => {
-                
+                let nbors = rt.graph.neighbors_slice(*node);
+                let nbor = nbors[*path as usize];
+                let len = nbors.len();
+
+                rt.run_direction = RTDirection::Traverse;
+                rt.set_node_ptr(&nbor);
+
+                if len - 1 < *path as usize {
+                    rt.push_instr(Instr::RET { node: rt.node_ptr, path: path + 1 });
+                }
             },
             Instr::SCP { .. } => {
                 rt.scope_st.pop();
@@ -149,17 +173,18 @@ impl Instr {
                 rt.data_st.push(*data);
             },
             Instr::LOA { addr } => {
-                let scope_addr = rt.scope_st[rt.scope_st.len() - rt.scope_st_ptr as usize];
-                rt.heap[(scope_addr + addr) as usize] = HeapKind::Num(*rt.data_st.last().unwrap());
+                let scope_addr = rt.scope_st[rt.scope_st.len() - 1 - rt.scope_st_ptr as usize];
+                unsafe { rt.heap[(scope_addr + addr) as usize] = HeapKind::Num(*rt.data_st.last().unwrap_unchecked()); }
             },
             Instr::CALL { node } => {
-                rt.node_ptr = *node;
+                rt.run_direction = RTDirection::Traverse;
+                rt.set_node_ptr(node);
             },
             Instr::IF {t, f, .. } => {
-                let cond = rt.data_st.pop().unwrap() != 0;
+                let cond = unsafe { rt.data_st.pop().unwrap_unchecked() != 0 };
 
                 rt.run_direction = RTDirection::Traverse;
-                rt.node_ptr = if cond { *t } else { *f };
+                rt.set_node_ptr(if cond { t } else { f });
             },
         }
     }
@@ -173,8 +198,8 @@ enum RTDirection {
     Traverse,
 }
 
-#[derive(Default)]
-enum HeapKind {
+#[derive(Default, Clone, Debug)]
+pub enum HeapKind {
     #[default]
     None,
     Err,
